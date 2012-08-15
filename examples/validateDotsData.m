4a59f26d1a8733aaf47293cdc3b4fabe23b726f4
Christopher Stawarz <cstawarz@mit.edu>
2012-08-15 11:03:10 -0400

    Modernized and improved validate_dots_data.py.  Added equivalent MATLAB function validateDotsData.m.

---
+++ examples/validateDotsData.m
@@ -0,0 +1,36 @@
+function validateDotsData(filename)
+
+codec = getCodecs(filename);
+codec = codec.codec;
+all_codes = [codec(:).code];
+
+for i = 1:length(codec)
+    if strcmp(codec(i).tagname, '#announceStimulus')
+        event_codes = [codec(i).code];
+        break;
+    end
+end
+
+events = getEvents(filename, event_codes);
+nEvents = 0;
+
+for i = 1:length(events)
+    value = events(i).data;
+    if isstruct(value) && strcmp(value.type, 'dynamic_random_dots') ...
+            && isfield(value, 'dots')
+        data = typecast(value.dots, 'single');
+        assert(length(data) == (2 * value.num_dots), ...
+               'Wrong number of dots');
+        
+        x = data(1:2:end) - value.field_center_x;
+        y = data(2:2:end) - value.field_center_y;
+        field_radius = value.field_radius;
+        assert(max(x.*x + y.*y) <= (field_radius*field_radius), ...
+               'Invalid dot coordinates');
+        
+        nEvents = nEvents + 1;
+    end
+end
+
+assert(nEvents > 0, 'No events processed');
+disp(sprintf('Processed %d events', nEvents));
--- examples/validate_dots_data.py
+++ examples/validate_dots_data.py
@@ -3,30 +3,26 @@ import numpy
 from mworks.data import MWKFile
 
 
-NUM_DOTS = 1000
-FIELD_RADIUS = 10
-
-
 def validate_dots_data(filename):
-    f = MWKFile(filename)
-    f.open()
-    try:
-        events = f.get_events(codes=['#announceStimulus'])
-        for i, data in enumerate(e.value['dots'] for e in events
-                                 if isinstance(e.value, dict) and
-                                 e.value['type'] == 'dynamic_random_dots'):
-            data = numpy.fromstring(data, numpy.float32)
-            assert len(data) == (2 * NUM_DOTS)
-            x = data[::2]
-            y = data[1::2]
-            assert (x*x + y*y).max() <= (FIELD_RADIUS * FIELD_RADIUS)
+    with MWKFile(filename) as f:
+        num_events = 0
+        for e in f.get_events_iter(codes=['#announceStimulus']):
+            value = e.value
+            if (isinstance(value, dict) and
+                (value['type'] == 'dynamic_random_dots') and
+                ('dots' in value)):
+                data = numpy.fromstring(value['dots'], numpy.float32)
+                assert len(data) == (2 * value['num_dots'])
+                
+                x = data[::2] - value['field_center_x']
+                y = data[1::2] - value['field_center_y']
+                field_radius = value['field_radius']
+                assert (x*x + y*y).max() <= (field_radius * field_radius)
+                
+                num_events += 1
 
-        assert i > 0
-        print 'Processed %d events' % i
-
-    finally:
-        f.close()
-        f.unindex()
+        assert num_events > 0
+        print 'Processed %d events' % num_events
 
 
 if __name__ == '__main__':
